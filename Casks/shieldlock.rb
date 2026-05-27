1addc265008f584d1ac99831fab708e8ca451aa1
Ben Dechrai <ben@dechrai.com>
2026-05-27 17:29:04 -0500

    Add shieldlock cask v1.0.0

---
+++ Casks/shieldlock.rb
@@ -0,0 +1,11 @@
+cask "shieldlock" do
+  version "1.0.0"
+  sha256 "9647f6854d9f02654ed8511900b170d8afcdf10d2331fe961e84c6742234b30d"
+
+  url "https://github.com/bendechrai/shieldlock/releases/download/v#{version}/ShieldLock.zip"
+  name "ShieldLock"
+  desc "Transparent screen locker for macOS"
+  homepage "https://github.com/bendechrai/shieldlock"
+
+  app "ShieldLock.app"
+end
