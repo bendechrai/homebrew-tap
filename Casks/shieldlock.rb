b03ce89781221d3dc0a7c108206f775348dcf530
Ben Dechrai <ben@dechrai.com>
2026-05-29 16:32:27 -0500

    Bump shieldlock cask to v1.0.2

--- Casks/shieldlock.rb
+++ Casks/shieldlock.rb
@@ -1,5 +1,5 @@
 cask "shieldlock" do
-  version "1.0.0"
+  version "1.0.2"
   sha256 "9647f6854d9f02654ed8511900b170d8afcdf10d2331fe961e84c6742234b30d"
 
   url "https://github.com/bendechrai/shieldlock/releases/download/v#{version}/ShieldLock.zip"
